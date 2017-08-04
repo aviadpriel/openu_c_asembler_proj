51465f236c4b11c3d3916871bf1b7507c51f305e
aviadpriel <aviad@aviad-priel>
2017-08-04 19:03:18 +0300

    sbat shalom

---
+++ evgeny/first_oprend.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#define ERROR -1
+typedef {ON,OFF,WAIT}SWITCHER;
+/*
+הפונקציה הזאת אמורה לברר מה היא שיטת המיעון 
+של הפונקצייה 
+וגם לברר את תקינות התווים בתוכם
+
+אם צריך 2 אופראנדים היא מפעילה פונקציה 
+second_oprend()
+בסוף מחשבת כמה מילות הפונקציה צורכת
+*/
+int firstOperend(char * buf,int functionIndex,int *ic);
+{
+    
+
+
+    
+}
