f3315360a610d144b58cd8957169771c101403d7
Evgeny <evgeny.ginzburg86@gmail.com>
2017-08-04 12:51:14 +0300

    first\second_run arguments changed as agreed.

--- evgeny/assembler.c
+++ evgeny/assembler.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#define LINE_LENGTH 81
 
-first_run(char []buff);
-second_run(char []buff);
+first_run(file);
+second_run(file);
 
 
 /*****************************************************************************************************************************************************
@@ -11,9 +10,9 @@ second_run(char []buff);
 	*Return: 0 on success
 	*Description: read assembly file and decodes it to machine language, Outputs 3 organized file_name.ob/ent/ext in a "strange 4-base" display. 
 *****************************************************************************************************************************************************/
-int main ( int argc, char *argv[] )
+int main ( int argc, char *argv[] )//לבדוק אם הוראה לקובץ יחיד או שניתן להכניס יותר - בסרטון של דני דובר על 2 קבצים יחד בשורת פקודה.
 {   
-    char buff[LINE_LENGTH];//לבדוק אם הוראה לקובץ יחיד או שניתן להכניס יותר - בסרטון של דני דובר על 2 קבצים יחד בשורת פקודה.
+    int ic = dc = error = 0;
     if ( argc != 2 ) /* argc should be 2 for correct execution */
     {
         /* We print argv[0] assuming it is the program name */
@@ -32,14 +31,17 @@ int main ( int argc, char *argv[] )
         }
         else /*begin the 2 stage processing*/
         {
-            while(fgets( buff, LINE_LENGTH, (FILE*)file ))
-                first_run(buff);
-            rewind(file);
-            while(fgets( buff, LINE_LENGTH, (FILE*)file ))
-                second_run(buff);
-            if(!ERROR)
+           error = first_run(file, &dc, &ic);
+            if(error)
             {
-                //free and clear all data without creating any file
+                //free and clear functions without creating any file
+                fclose(file);
+            }
+            rewind(file);
+            second_run(file, &dc, &ic);
+            if(error)
+            {
+                //free and clear functions without creating any file
                 fclose(file);
             }   
             //create the needed files and free and clear data.
