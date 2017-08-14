63f017dd1062b6442cf7203209a04ddaba80bf91
aviadpriel <aviad@aviad-priel>
2017-08-14 11:54:40 +0300

    /

--- aviad/prog_struct.c
+++ aviad/prog_struct.c
@@ -7,7 +7,141 @@
 
 
 typedef enum {ON,OFF,WAIT} SWITCHER;
+typedef struct labelsList {#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define WORD_SIZE 10
+#define ERROR -1
+#define LABEL_MAX_LEN 30
+
+
+typedef enum {ON,OFF,WAIT} SWITCHER;
+
+typedef struct psw
+{
+  int era:2;
+  int op1:2;
+  int op2:2;
+  int opcode:4;
+}psw;
+typedef struct decode
+{
+    psw PSW;
+    int data:WORD_SIZE;
+    struct decode* next; 
+}decode;
 typedef struct labelsList {
+  char *label;
+SWITCHER data;  
+SWITCHER action;
+SWITCHER external;
+int address;
+struct labelsList *next;
+} labelsList;
+
+typedef struct dataList{
+int data:WORD_SIZE;
+struct dataList *next;
+}dataList;
+typedef struct commandList{
+char *name;  
+int operends;
+int operendGroup;
+}commandList;
+
+dataList* newData(int data);
+
+/************dataList functions*******/
+dataList* newData(int data)
+{
+  dataList *p = (dataList*)malloc(sizeof(dataList));
+  if(!p)
+  {
+    printf("mermory alloction error\n");
+    exit(1);
+  }
+  p->data = data;
+  p->next=NULL;
+  return p;
+}
+
+
+void insertData(dataList **dataHead,int data)
+{
+   dataList *current=(*dataHead);
+  if(!(*dataHead))
+  {
+  (*dataHead)=newData(data);
+
+  }
+  else
+    {
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next=newData(data);
+    }
+}
+
+
+/************labelsList functions*******/
+labelsList* newLabel(char *label,int address,SWITCHER action,SWITCHER external,SWITCHER data)
+{
+  labelsList *p = (labelsList*)malloc(sizeof(labelsList));
+  if (!p) {
+    printf("memmory error exit\n");
+    exit(1);
+  }
+  p->label=(char *)malloc(sizeof(char)*LABEL_MAX_LEN);
+  strncpy(p->label,label,LABEL_MAX_LEN);
+  p->action=action;
+  p->external=external;
+  p->address=address;
+  p->data=data;
+  p->next =NULL;
+  return p;
+}
+
+int addLabel(labelsList **labelsHead, char *label,SWITCHER action,SWITCHER external,
+  SWITCHER data,int address,int line)
+{
+/* insert using a loop and pointer to pointer*/
+  while(*labelsHead)
+  {
+     if(strcmp(label,(*labelsHead)->label)!=0)
+       labelsHead = &( (*labelsHead)->next);
+     else/*error the label is in the list */
+    {
+      printf("Error: line %d:the label %s is in the list \n",line,label);
+      return ERROR;
+    }
+  }
+  *labelsHead = newLabel(label,address,action,external,data);
+  return 1;
+}
+
+void freeDataList(dataList *dataHead)
+{
+  dataList* temp;
+    while (dataHead != NULL)
+    { 
+        temp = dataHead; 
+        temp ->next=NULL;
+        dataHead = dataHead->next;
+        free(temp);
+    }
+}
+void freeLabelsList(labelsList *labelsHead)
+{
+  labelsList* temp;
+    while (labelsHead != NULL)
+    { 
+        temp = labelsHead; 
+        temp ->next=NULL;
+        labelsHead = labelsHead->next;
+        free(temp);
+    }
+}
   char *label;
 SWITCHER data;  
 SWITCHER action;
