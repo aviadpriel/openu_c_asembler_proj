270190d532bdfc5d182f59f3b9da29b99e98363e
Evgeny <evgeny.ginzburg86@gmail.com>
2017-08-15 10:46:03 +0300

    binary machine code struct definition: 1.machine_code_node for psw decoding 2. word- node for extra words for psw binary expansion

--- evgeny&aviad/general.h
+++ evgeny&aviad/general.h
@@ -7,6 +7,28 @@ what to include inside wrapper?
 #ifndef GENERAL_H
 #define GENERAL_H
 
-//write evreythig needed for genreal header
+enum symbol_flag {}
+/*****************************************************************************************************************************************************
+
+*****************************************************************************************************************************************************/
+typedef struct machine_code_node
+{
+    char psw[WORD_SIZE];/*assemblers base line*/
+    machine_code *next;/*pointer to next assemblers base line*/
+    binWord *nextDown;/*pointer to next word (if needed) related to this psw */
+    int commandLineCnt;/*number line of the original file line*/
+
+}machine_code_node;
+
+/*****************************************************************************************************************************************************
+
+*****************************************************************************************************************************************************/
+typedef struct word 
+{
+char wordbase[WORD_SIZE];
+int is_encoded : 1;/*1- this word is already decoden to binary number, 0- not decoded yet*/
+word *next;/*next word related to the psw (if needed)*/
+int IC;
+}word;
 
 #endif /*end of GENERAL_H*/
